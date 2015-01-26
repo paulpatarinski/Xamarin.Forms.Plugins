dc9526cc3373887f486d374adbfce8d835e772e8
ppatarinski <ppatarinski@paylocity.com>
2015-01-25 18:16:01 -0600

    Adding pin images

---
+++ ExtendedMap/SampleApp/SampleApp/Helpers/Icons.cs
@@ -0,0 +1,12 @@
+﻿namespace SampleApp.Helpers
+{
+  /// <summary>
+  /// Put the icon images in the Assets directory for Android
+  /// </summary>
+  public static class Icons
+  {
+    public static string BeachVball { get { return "BeachVball"; } }
+
+    public static string IndoorVball { get { return "IndoorVball"; } }
+  }
+}
