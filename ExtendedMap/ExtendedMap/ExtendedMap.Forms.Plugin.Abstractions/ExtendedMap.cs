1fee2cc8a8e870749a37851d7b59771c839f135c
ppatarinski <ppatarinski@paylocity.com>
2015-01-25 19:52:36 -0600

    Making the CameraOffset a bindable property

--- ExtendedMap/ExtendedMap/ExtendedMap.Forms.Plugin.Abstractions/ExtendedMap.cs
+++ ExtendedMap/ExtendedMap/ExtendedMap.Forms.Plugin.Abstractions/ExtendedMap.cs
@@ -26,31 +26,6 @@ namespace ExtendedMap.Forms.Plugin.Abstractions
       }
 		}
 
-		string _selectedPinAddress;
-
-		public string SelectedPinAddress {
-			get{ return _selectedPinAddress; }
-			set {
-				_selectedPinAddress = value;
-
-				var selectedPin = CustomPins.FirstOrDefault (x => x.Address.Equals (value)) as ExtendedPin;
-
-				var parent = this.Parent.Parent as CustomMapContentView;
-
-				if (parent == null)
-					throw new Exception ("Not able to retrieve the parent of the ExtendedMap");
-
-				//Set the Expanded to Expanded (otherwise for some reason the footer minimizes)
-				if (parent.FooterMode == FooterMode.Expanded) {
-					parent.FooterMode = FooterMode.Expanded;
-				}
-
-				CenterOnPosition = selectedPin.Position;
-
-				base.SetValue (SelectedPinProperty, selectedPin);
-			}
-		}
-
 		public static readonly BindableProperty SelectedPinProperty = BindableProperty.Create<ExtendedMap, ExtendedPin> (x => x.SelectedPin, new ExtendedPin ());
 
 		public ExtendedPin SelectedPin {
@@ -58,13 +33,52 @@ namespace ExtendedMap.Forms.Plugin.Abstractions
 		}
 
 		public static readonly BindableProperty CustomPinsProperty = BindableProperty.Create<ExtendedMap, ObservableCollection<ExtendedPin>> (x => x.CustomPins, new ObservableCollection<ExtendedPin> (){ new ExtendedPin (){ Label = "test123" } });
-		private Position _centerOnPosition;
 
 		public ObservableCollection<ExtendedPin> CustomPins {
 			get{ return (ObservableCollection<ExtendedPin>)base.GetValue (CustomPinsProperty); }
 			set{ base.SetValue (CustomPinsProperty, value); }
 		}
 
-		public int CameraFocusYOffset { get; set; }
+    public static readonly BindableProperty CameraFocusYOffsetProperty = BindableProperty.Create("CameraFocusYOffsetProperty", typeof(int), typeof(ExtendedMap), default(int));
+
+    public int CameraFocusYOffset
+    {
+      get
+      {
+        return (int)GetValue(CameraFocusYOffsetProperty);
+      }
+      set
+      {
+        SetValue(CameraFocusYOffsetProperty, value);
+      }
+    }
+
+    string _selectedPinAddress;
+
+    public string SelectedPinAddress
+    {
+      get { return _selectedPinAddress; }
+      set
+      {
+        _selectedPinAddress = value;
+
+        var selectedPin = CustomPins.FirstOrDefault(x => x.Address.Equals(value)) as ExtendedPin;
+
+        var parent = this.Parent.Parent as CustomMapContentView;
+
+        if (parent == null)
+          throw new Exception("Not able to retrieve the parent of the ExtendedMap");
+
+        //Set the Expanded to Expanded (otherwise for some reason the footer minimizes)
+        if (parent.FooterMode == FooterMode.Expanded)
+        {
+          parent.FooterMode = FooterMode.Expanded;
+        }
+
+        CenterOnPosition = selectedPin.Position;
+
+        base.SetValue(SelectedPinProperty, selectedPin);
+      }
+    }
 	}
 }
