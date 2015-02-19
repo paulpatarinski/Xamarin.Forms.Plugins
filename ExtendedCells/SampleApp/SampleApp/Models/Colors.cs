4e8e1592fc8581777b87c9a11eef9ea692de6368
Paul Patarinski <paulpatarinski@gmail.com>
2015-02-19 15:05:14 -0600

    Adding  colors struct

---
+++ ExtendedCells/SampleApp/SampleApp/Models/Colors.cs
@@ -0,0 +1,22 @@
+﻿using Xamarin.Forms;
+
+namespace SampleApp.Models
+{
+  public static class Colors
+  {
+    public static Color DefaultColor
+    {
+      get
+      {
+        var color = Color.White;
+
+        Device.OnPlatform(() =>
+        {
+          color = Color.Black;
+        });
+
+        return color;
+      }
+    }
+  }
+}
