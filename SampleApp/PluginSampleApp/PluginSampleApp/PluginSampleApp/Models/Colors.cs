4a226bb9daf51734b63a1b97988a2e0f9db7b792
paylocity-blackcobra <mobileappcreator@ITs-MacBook-Pro.local>
2015-02-19 19:50:04 -0600

    Adding a colors struct upgrading to the latest version of the ExtendedCell

---
+++ SampleApp/PluginSampleApp/PluginSampleApp/PluginSampleApp/Models/Colors.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace PluginSampleApp
+{
+	public static class Colors
+	{
+		public static Color DefaultColor {
+			get {
+				var color = Color.White;
+
+				Device.OnPlatform (() => {
+					color = Color.Black;
+				});
+
+				return color;
+			}
+		}
+
+		public static Color BlackOrWhite {
+			get {
+				var color = Color.Black;
+
+				Device.OnPlatform (() => {
+					color = Color.White;
+				});
+
+				return color;
+			}
+		}
+	}
+}
+
