76b16297c65c7c9b804344d443aa22d72a7ad34c
paylocity-blackcobra <mobileappcreator@ITs-MacBook-Pro.local>
2015-02-21 14:11:19 -0600

    Adding a Page that demonstrates using Resources

--- SVG/SampleApp/SampleApp/App.cs
+++ SVG/SampleApp/SampleApp/App.cs
@@ -3,31 +3,32 @@ using Xamarin.Forms;
 
 namespace SampleApp
 {
-    public class App : Application
-    {
-        public App()
-        {
-          var tabbedPage = new TabbedPage();
+	public class App : Application
+	{
+		public App ()
+		{
+			var tabbedPage = new TabbedPage ();
 
-          tabbedPage.Children.Add(new ViaCode());
-          tabbedPage.Children.Add(new ViaXaml());
+			tabbedPage.Children.Add (new ViaCode ());
+			tabbedPage.Children.Add (new ViaXaml ());
+			tabbedPage.Children.Add (new ViaResources ());
 
-          MainPage = tabbedPage;
-        }
+			MainPage = tabbedPage;
+		}
 
-        protected override void OnStart()
-        {
-            // Handle when your app starts
-        }
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
 
-        protected override void OnSleep()
-        {
-            // Handle when your app sleeps
-        }
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
 
-        protected override void OnResume()
-        {
-            // Handle when your app resumes
-        }
-    }
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
 }
---
+++ SVG/SampleApp/SampleApp/Pages/ViaResources.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using SampleApp.ViewModel;
+
+namespace SampleApp
+{
+	public partial class ViaResources : ContentPage
+	{
+		public ViaResources ()
+		{
+			InitializeComponent ();
+			BindingContext = new ViaXamlViewModel ();
+		}
+	}
+}
+
