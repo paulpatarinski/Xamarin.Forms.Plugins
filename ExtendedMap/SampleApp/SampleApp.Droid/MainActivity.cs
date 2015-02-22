5474c131ad2a83c7fbca006e4e655d06ef5fb8d5
paylocity-blackcobra <mobileappcreator@ITs-MacBook-Pro.local>
2015-02-22 15:05:32 -0600

    Removing the user pref from source control, and renaming the app

--- ExtendedMap/SampleApp/SampleApp.Droid/MainActivity.cs
+++ ExtendedMap/SampleApp/SampleApp.Droid/MainActivity.cs
@@ -8,21 +8,21 @@ using Xamarin;
 
 namespace SampleApp.Droid
 {
-    [Activity(Label = "SampleApp", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
-    {
-        protected override void OnCreate(Bundle bundle)
-        {
-            base.OnCreate(bundle);
+	[Activity (MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
 
-            global::Xamarin.Forms.Forms.Init(this, bundle);
-            FormsMaps.Init(this,bundle);
-            SvgImageRenderer.Init();
-            TwoColumnCellRenderer.Init();
-            ExtendedMapRenderer.Init();
+			global::Xamarin.Forms.Forms.Init (this, bundle);
+			FormsMaps.Init (this, bundle);
+			SvgImageRenderer.Init ();
+			TwoColumnCellRenderer.Init ();
+			ExtendedMapRenderer.Init ();
 
-            LoadApplication(new App());
-        }
-    }
+			LoadApplication (new App ());
+		}
+	}
 }
 
