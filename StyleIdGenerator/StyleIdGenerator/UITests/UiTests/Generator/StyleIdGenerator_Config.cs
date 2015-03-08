474d9c629d8cfbc795777670e0dbd9b4e1148d18
paulpatarinski <paulpatarinski@gmail.com>
2015-03-08 17:26:46 -0600

    Adding a StyleIdGenerator_Config

---
+++ StyleIdGenerator/StyleIdGenerator/UITests/UiTests/Generator/StyleIdGenerator_Config.cs
@@ -0,0 +1,24 @@
+﻿
+
+
+
+
+	public struct StyleIds
+	{
+					public struct Page1
+			{
+				
+				public static string lblMainText = "lblMainText";
+				
+				public static string lblWithSpace = "lbl With Space";
+				
+				public static string LBLALLCAPS = "LBLALLCAPS";
+							}
+					public struct Page2
+			{
+				
+				public static string lblPage2 = "lblPage2";
+							}
+			}
+
+
--- StyleIdGenerator/StyleIdGenerator/UITests/UiTests/Page1Tests.cs
+++ StyleIdGenerator/StyleIdGenerator/UITests/UiTests/Page1Tests.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using NUnit.Framework;
 using UiTests.ExtensionsMethods;
 
@@ -32,5 +34,49 @@ namespace UiTests
             Assert.IsNotNull(lblAllCaps);
             Assert.IsTrue(lblAllCaps.Any());
         }
+
+        static IEnumerable<object> GetMatchingXamlFiles(string path)
+        {
+            //String search code from here https://msdn.microsoft.com/en-us/library/bb882639.aspx
+            IEnumerable<System.IO.FileInfo> fileList = new List<FileInfo>();
+
+            // Create the regular expression to find all things "Visual".
+            var searchTerm =
+            new System.Text.RegularExpressions.Regex("StyleId *= *\"([^\"]*?)\"");
+            var replaceTerm =
+            new System.Text.RegularExpressions.Regex("StyleId *= *");
+
+            // Search the contents of each .htm file.
+            // Remove the where clause to find even more matchedValues!
+            // This query produces a list of files where a match
+            // was found, and a list of the matchedValues in that file.
+            // Note: Explicit typing of "Match" in select clause.
+            // This is required because MatchCollection is not a
+            // generic IEnumerable collection.
+            var queryMatchingFiles =
+                    from file in fileList
+                    where file.Extension == ".xaml"
+                    let fileText = System.IO.File.ReadAllText(file.FullName)
+                    let matches = searchTerm.Matches(fileText)
+                    where matches.Count > 0
+                    select new MatchingFile
+                    {
+                        name = file.FullName,
+                        nameWithoutExtension = file.Name.Replace(".xaml", ""),
+                        matchedValues = from System.Text.RegularExpressions.Match match in matches
+                                        select replaceTerm.Replace(match.Value, "").Replace("\"", "")
+                    };
+
+            return queryMatchingFiles;
+        }
+
+
+    }
+
+    internal class MatchingFile
+    {
+        public string name { get; set; }
+        public string nameWithoutExtension { get; set; }
+        public IEnumerable<string> matchedValues { get; set; }
     }
 }
