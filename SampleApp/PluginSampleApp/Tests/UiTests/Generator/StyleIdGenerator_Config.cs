3993c871b188a1e91697036eab6c356ebc539c2f
paulpatarinski <paulpatarinski@gmail.com>
2015-03-08 18:52:18 -0600

    Adding the StyleId Generator nuget to the Sample Solution

---
+++ SampleApp/PluginSampleApp/Tests/UiTests/Generator/StyleIdGenerator_Config.cs
@@ -0,0 +1,19 @@
+﻿
+
+
+
+
+	public struct StyleIds
+	{
+					public struct TwoColumnCellListview
+			{
+				
+				public static string cellListview = "cellListview";
+							}
+					public struct TwoColumnCellTableView
+			{
+				
+				public static string stackLayoutCellTableView = "stackLayoutCellTableView";
+							}
+			}
+
