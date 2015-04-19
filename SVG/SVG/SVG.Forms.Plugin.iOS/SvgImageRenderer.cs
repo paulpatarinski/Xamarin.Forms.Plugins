3373afb04f2bf7a286ce983e5547772b868e09c6
Paul Patarinski <paulpatarinski@gmail.com>
2015-04-18 21:21:14 -0500

    Adding an implementation for the SvgImageRenderer for iOS, which uses NGraphics

--- SVG/SVG/SVG.Forms.Plugin.iOS/SvgImageRenderer.cs
+++ SVG/SVG/SVG.Forms.Plugin.iOS/SvgImageRenderer.cs
@@ -1,5 +1,13 @@
 ﻿using Xamarin.Forms;
 using SVG.Forms.Plugin.iOS;
+using Xamarin.Forms.Platform.iOS;
+using System.IO;
+using SVG.Forms.Plugin.Abstractions;
+using NGraphics;
+using UIKit;
+using NGraphics.Parsers;
+using System;
+using CoreGraphics;
 
 [assembly: ExportRenderer(typeof(SVG.Forms.Plugin.Abstractions.SvgImage), typeof(SvgImageRenderer))]
 namespace SVG.Forms.Plugin.iOS
@@ -7,11 +15,48 @@ namespace SVG.Forms.Plugin.iOS
     /// <summary>
     /// SVG Renderer
     /// </summary>
-    public class SvgImageRenderer //: TRender (replace with renderer type
+	public class SvgImageRenderer :  ImageRenderer
     {
         /// <summary>
         /// Used for registration with dependency service
         /// </summary>
         public static void Init() { }
+
+		protected override void OnElementChanged (ElementChangedEventArgs<Image> e)
+		{
+			base.OnElementChanged (e);
+
+			var svgImage = (SvgImage)Element;
+
+			var svgStream = svgImage.SvgAssembly.GetManifestResourceStream(svgImage.SvgPath);
+
+			if (svgStream == null) {
+				throw new Exception (string.Format ("Error retrieving {0} make sure Build Action is Embedded Resource", svgImage.SvgPath));
+			}
+
+			var r = new SvgReader(new StreamReader(svgStream), new StylesParser(new ValuesParser()),new ValuesParser());
+
+			var graphics = r.Graphic;
+
+			var width = svgImage.WidthRequest == 0 ? 100 : svgImage.WidthRequest;
+			var height = svgImage.HeightRequest == 0 ? 100 : svgImage.HeightRequest;
+
+			var scale = 1.0;
+
+			if (height >= width) {
+					scale = height / graphics.Size.Height;
+			} else {
+				scale = width / graphics.Size.Width;
+			}
+
+			var scaleFactor = UIScreen.MainScreen.Scale;
+
+			var canvas = new ApplePlatform().CreateImageCanvas(graphics.Size, scale * scaleFactor);
+			graphics.Draw(canvas);
+			var image = canvas.GetImage ();
+
+			var uiImage = image.GetUIImage ();
+			Control.Image = uiImage;
+		}
     }
 }
