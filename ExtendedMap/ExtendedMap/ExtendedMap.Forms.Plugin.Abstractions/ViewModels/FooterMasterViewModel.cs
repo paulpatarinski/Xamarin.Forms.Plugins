81cc6e00302d65d3900a32ce5b71d7fdf2ccde0a
Paul Patarinski <paulpatarinski@gmail.com>
2015-07-21 04:03:14 -0500

    Adding FooterMaster View and ViewModel for later usage

---
+++ ExtendedMap/ExtendedMap/ExtendedMap.Forms.Plugin.Abstractions/ViewModels/FooterMasterViewModel.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using ExtendedMap.Forms.Plugin.Abstractions.Models;
+using System.Reflection;
+using System.ComponentModel;
+using System.Collections.Generic;
+using Xamarin.Forms.Maps;
+
+namespace ExtendedMap.Forms.Plugin.Abstractions.ViewModels
+{
+	public class FooterMasterViewModel : INotifyPropertyChanged
+	{
+		public FooterMasterViewModel (ExtendedMap extendedMap)
+		{
+			_extendedMap = extendedMap;
+			Address = extendedMap.SelectedPin.Address;
+			SvgAssembly = typeof(CustomMapContentView).GetTypeInfo ().Assembly;
+//
+//				var navImage = new SvgImage {
+//				SvgPath = string.Format ("ExtendedMap.Forms.Plugin.Abstractions.Images.{0}", buttonImage),
+//				SvgAssembly = typeof(CustomMapContentView).GetTypeInfo ().Assembly,
+//				HorizontalOptions = LayoutOptions.Center,
+//				HeightRequest = height,
+//				WidthRequest = width
+//			};
+
+		}
+
+		ExtendedMap _extendedMap;
+
+		ExtendedPin _sPin;
+
+		public string Address { get; set;
+		}
+
+		public ExtendedPin SPin {
+			get{
+				if(_sPin == null)
+				{
+					_sPin = _extendedMap.SelectedPin;
+				}
+
+				return _sPin;
+			}
+			set{
+				_sPin = value;
+				OnPropertyChanged ("SPin");
+			}
+		}
+
+		public Assembly SvgAssembly;
+
+		#region INotifyPropertyChanged implementation
+
+		// boiler-plate
+		public event PropertyChangedEventHandler PropertyChanged;
+		protected virtual void OnPropertyChanged(string propertyName)
+		{
+			PropertyChangedEventHandler handler = PropertyChanged;
+			if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+		}
+		protected bool SetField<T>(ref T field, T value, string propertyName)
+		{
+			if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+			field = value;
+			OnPropertyChanged(propertyName);
+			return true;
+		}
+		#endregion
+
+}
+}
+
---
+++ ExtendedMap/ExtendedMap/ExtendedMap.Forms.Plugin.Abstractions/Views/FooterMasterContentView.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using ExtendedMap.Forms.Plugin.Abstractions.Services;
+using ExtendedMap.Forms.Plugin.Abstractions.ViewModels;
+
+namespace ExtendedMap.Forms.Plugin.Abstractions.Views
+{
+	public partial class FooterMasterContentView : ContentView
+	{
+		public FooterMasterContentView(ExtendedMap extendedMap, UIHelper uiHelper, Footer footer)
+		{
+			_extendedMap = extendedMap;
+			_uiHelper = uiHelper;
+			_footer = footer;
+
+						BindingContext = new FooterMasterViewModel(extendedMap) ;
+//			BindingContext = extendedMap ;
+//			Content = CreateFooter ();
+			InitializeComponent ();
+		}
+
+		ExtendedMap _extendedMap;
+		UIHelper _uiHelper;
+		Footer _footer;
+
+
+	}
+}
+
