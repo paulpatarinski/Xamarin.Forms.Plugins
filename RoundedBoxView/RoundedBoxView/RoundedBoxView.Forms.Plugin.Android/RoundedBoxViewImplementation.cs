e907f07d86efc3e8724eff0634e0e95065b5ecfc
Paul Patarinski <paulpatarinski@gmail.com>
2015-07-24 08:07:49 -0500

    Cleaning up the android implementation

--- RoundedBoxView/RoundedBoxView/RoundedBoxView.Forms.Plugin.Android/RoundedBoxViewImplementation.cs
+++ RoundedBoxView/RoundedBoxView/RoundedBoxView.Forms.Plugin.Android/RoundedBoxViewImplementation.cs
@@ -1,46 +1,42 @@
-using System.ComponentModel;
-using Android.Graphics;
+using Android.Graphics.Drawables;
 using RoundedBoxView.Forms.Plugin.Droid;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.Android;
-using System;
-using Android.Graphics.Drawables.Shapes;
-using Android.Graphics.Drawables;
+using View = Android.Views.View;
 
+[assembly:
+  ExportRenderer(typeof (RoundedBoxView.Forms.Plugin.Abstractions.RoundedBoxView), typeof (RoundedBoxViewRenderer))]
 
-[assembly: ExportRenderer(typeof(RoundedBoxView.Forms.Plugin.Abstractions.RoundedBoxView), typeof(RoundedBoxViewRenderer))]
 namespace RoundedBoxView.Forms.Plugin.Droid
 {
-    /// <summary>
-    /// Source from https://gist.github.com/rudyryk/8cbe067a1363b45351f6
-    /// </summary>
-	public class RoundedBoxViewRenderer : ViewRenderer<RoundedBoxView.Forms.Plugin.Abstractions.RoundedBoxView, Android.Views.View>
+  public class RoundedBoxViewRenderer : ViewRenderer<Abstractions.RoundedBoxView, View>
+  {
+    public static void Init()
     {
-        public static void Init() { }
-
-		private RoundedBoxView.Forms.Plugin.Abstractions.RoundedBoxView _formControl {
-			get{
-				return Element as RoundedBoxView.Forms.Plugin.Abstractions.RoundedBoxView;
-			}
-		}
-
-		protected override void OnElementChanged(ElementChangedEventArgs<RoundedBoxView.Forms.Plugin.Abstractions.RoundedBoxView> e)
-        {
-            base.OnElementChanged(e);
-
-			if (_formControl == null)
-				return;
-
-			var relativeCornerRadius = (float)(_formControl.CornerRadius * 3.7);
-			var relativeBorderThickness = _formControl.BorderThickness * 3;
-
-			var shape = new GradientDrawable ();
-
-			shape.SetCornerRadius (relativeCornerRadius);
-			shape.SetColor (_formControl.BackgroundColor.ToAndroid ());
-			shape.SetStroke (relativeBorderThickness, _formControl.BorderColor.ToAndroid ());
-
-			Background = shape;
-        }
     }
+
+    private Abstractions.RoundedBoxView _formControl
+    {
+      get { return Element; }
+    }
+
+    protected override void OnElementChanged(ElementChangedEventArgs<Abstractions.RoundedBoxView> e)
+    {
+      base.OnElementChanged(e);
+
+      if (_formControl == null)
+        return;
+
+      var relativeCornerRadius = (float) (_formControl.CornerRadius*3.7);
+      var relativeBorderThickness = _formControl.BorderThickness*3;
+
+      var shape = new GradientDrawable();
+
+      shape.SetCornerRadius(relativeCornerRadius);
+      shape.SetColor(_formControl.BackgroundColor.ToAndroid());
+      shape.SetStroke(relativeBorderThickness, _formControl.BorderColor.ToAndroid());
+
+      Background = shape;
+    }
+  }
 }
