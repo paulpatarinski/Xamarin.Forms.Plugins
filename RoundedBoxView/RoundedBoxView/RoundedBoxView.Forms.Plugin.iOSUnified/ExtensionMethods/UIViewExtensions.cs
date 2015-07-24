c2590fec79d77b3d952fc6b1e517f54d99710ed5
Paul Patarinski <paulpatarinski@gmail.com>
2015-07-24 10:36:33 -0500

    Cleaning up the iOS implementation

--- RoundedBoxView/RoundedBoxView/RoundedBoxView.Forms.Plugin.iOSUnified/ExtensionMethods/UIViewExtensions.cs
+++ RoundedBoxView/RoundedBoxView/RoundedBoxView.Forms.Plugin.iOSUnified/ExtensionMethods/UIViewExtensions.cs
@@ -1,46 +1,47 @@
 ﻿using UIKit;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.iOS;
-using System;
 
 namespace RoundedBoxView.Forms.Plugin.iOSUnified.ExtensionMethods
 {
-	public static class UIViewExtensions
-	{
-		public static void UpdateFrom(this UIView nativeControl, RoundedBoxView.Forms.Plugin.Abstractions.RoundedBoxView formsControl, string propertyChanged = null )
-		{
-			if (nativeControl == null || formsControl == null)
-				return;
-			
-			if(propertyChanged == null)
-			{
-				nativeControl.Layer.MasksToBounds = true;
-				nativeControl.Layer.CornerRadius = (float) formsControl.CornerRadius;
-				nativeControl.UpdateBorder (formsControl.BorderColor, formsControl.BorderThickness);
-			}
-			else
-			{
-				if (propertyChanged == Abstractions.RoundedBoxView.CornerRadiusProperty.PropertyName)
-				{
-					nativeControl.Layer.CornerRadius = (float) formsControl.CornerRadius;
-				}
+  public static class UIViewExtensions
+  {
+    public static void InitializeFrom(this UIView nativeControl, Abstractions.RoundedBoxView formsControl)
+    {
+      if (nativeControl == null || formsControl == null)
+        return;
 
-				if (propertyChanged == Abstractions.RoundedBoxView.BorderColorProperty.PropertyName) 
-				{
-					nativeControl.UpdateBorder (formsControl.BorderColor, formsControl.BorderThickness);
-				}
+      nativeControl.Layer.MasksToBounds = true;
+      nativeControl.Layer.CornerRadius = (float) formsControl.CornerRadius;
+      nativeControl.UpdateBorder(formsControl.BorderColor, formsControl.BorderThickness);
+    }
 
-				if (propertyChanged == Abstractions.RoundedBoxView.BorderThicknessProperty.PropertyName) 
-				{
-					nativeControl.UpdateBorder (formsControl.BorderColor, formsControl.BorderThickness);
-				}
-			}
-		}
+    public static void UpdateFrom(this UIView nativeControl, Abstractions.RoundedBoxView formsControl,
+      string propertyChanged)
+    {
+      if (nativeControl == null || formsControl == null)
+        return;
 
-		public static void UpdateBorder(this UIView nativeControl, Color color, int thickness)
-		{
-			nativeControl.Layer.BorderColor = color.ToCGColor();
-			nativeControl.Layer.BorderWidth = (nfloat)thickness;
-		}
-	}
+      if (propertyChanged == Abstractions.RoundedBoxView.CornerRadiusProperty.PropertyName)
+      {
+        nativeControl.Layer.CornerRadius = (float) formsControl.CornerRadius;
+      }
+
+      if (propertyChanged == Abstractions.RoundedBoxView.BorderColorProperty.PropertyName)
+      {
+        nativeControl.UpdateBorder(formsControl.BorderColor, formsControl.BorderThickness);
+      }
+
+      if (propertyChanged == Abstractions.RoundedBoxView.BorderThicknessProperty.PropertyName)
+      {
+        nativeControl.UpdateBorder(formsControl.BorderColor, formsControl.BorderThickness);
+      }
+    }
+
+    public static void UpdateBorder(this UIView nativeControl, Color color, int thickness)
+    {
+      nativeControl.Layer.BorderColor = color.ToCGColor();
+      nativeControl.Layer.BorderWidth = thickness;
+    }
+  }
 }
--- RoundedBoxView/RoundedBoxView/RoundedBoxView.Forms.Plugin.iOSUnified/RoundedBoxViewImplementation.cs
+++ RoundedBoxView/RoundedBoxView/RoundedBoxView.Forms.Plugin.iOSUnified/RoundedBoxViewImplementation.cs
@@ -1,45 +1,43 @@
 ﻿using System.ComponentModel;
 using RoundedBoxView.Forms.Plugin.iOSUnified;
+using RoundedBoxView.Forms.Plugin.iOSUnified.ExtensionMethods;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.iOS;
-using RoundedBoxView.Forms.Plugin.iOSUnified.ExtensionMethods;
 
 [assembly:
-    ExportRenderer(typeof (RoundedBoxView.Forms.Plugin.Abstractions.RoundedBoxView), typeof (RoundedBoxViewRenderer))]
+  ExportRenderer(typeof (RoundedBoxView.Forms.Plugin.Abstractions.RoundedBoxView), typeof (RoundedBoxViewRenderer))]
 
 namespace RoundedBoxView.Forms.Plugin.iOSUnified
 {
+  /// <summary>
+  ///   Source From : https://gist.github.com/rudyryk/8cbe067a1363b45351f6
+  /// </summary>
+  public class RoundedBoxViewRenderer : BoxRenderer
+  {
     /// <summary>
-    /// Source From : https://gist.github.com/rudyryk/8cbe067a1363b45351f6
+    ///   Used for registration with dependency service
     /// </summary>
-    public class RoundedBoxViewRenderer : BoxRenderer
+    public static void Init()
     {
-        /// <summary>
-        ///     Used for registration with dependency service
-        /// </summary>
-        public static void Init()
-        {
-        }
-
-		private RoundedBoxView.Forms.Plugin.Abstractions.RoundedBoxView _formControl {
-			get{
-				return Element as RoundedBoxView.Forms.Plugin.Abstractions.RoundedBoxView;
-			}
-		}
-
-        protected override void OnElementChanged(ElementChangedEventArgs<BoxView> e)
-        {
-            base.OnElementChanged(e);
-
-			this.UpdateFrom (_formControl);
-
-        }
-
-        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
-        {
-            base.OnElementPropertyChanged(sender, e);
-
-			this.UpdateFrom (_formControl);
-        }
     }
+
+    private Abstractions.RoundedBoxView _formControl
+    {
+      get { return Element as Abstractions.RoundedBoxView; }
+    }
+
+    protected override void OnElementChanged(ElementChangedEventArgs<BoxView> e)
+    {
+      base.OnElementChanged(e);
+
+      this.InitializeFrom(_formControl);
+    }
+
+    protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+    {
+      base.OnElementPropertyChanged(sender, e);
+
+      this.UpdateFrom(_formControl, e.PropertyName);
+    }
+  }
 }
