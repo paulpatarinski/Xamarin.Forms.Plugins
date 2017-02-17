367363b2b861a3e96d9f7e4f2fdd97073d7a53ad
Benjamin <benjamin.laborier@metsia.fr>
2017-02-17 17:10:47 +0100

    Update to Xamarin 2.3.3.180 & Remove iOS classic (obsolete)

--- RoundedBoxView/SampleApp/SampleApp/App.xaml.cs
+++ RoundedBoxView/SampleApp/SampleApp/App.xaml.cs
@@ -1,33 +1,29 @@
-﻿using System.Collections.Generic;
-using SampleApp.Pages;
-using Xamarin.Forms;
+﻿using SampleApp.Pages;
 
 namespace SampleApp
 {
-  public partial class App : Application
-  {
-    public App()
+    using Xamarin.Forms;
+
+    public partial class App : Application
     {
-      InitializeComponent();
+        public App()
+        {
+            MainPage = new RoundedBoxViewPage();
+        }
 
-      MainPage = new RoundedBoxViewPage();
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
     }
-
-
-
-    protected override void OnStart()
-    {
-      // Handle when your app starts
-    }
-
-    protected override void OnSleep()
-    {
-      // Handle when your app sleeps
-    }
-
-    protected override void OnResume()
-    {
-      // Handle when your app resumes
-    }
-  }
 }
--- RoundedBoxView/SampleApp/SampleApp/Pages/RoundedBoxViewPage.xaml.cs
+++ RoundedBoxView/SampleApp/SampleApp/Pages/RoundedBoxViewPage.xaml.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 
 namespace SampleApp.Pages
 {
-    public partial class RoundedBoxViewPage : ContentPage
+    public partial class RoundedBoxViewPage
     {
         public RoundedBoxViewPage()
         {
