76f82aa3dab7e08446710b8ca393d709359191c7
Ian Nájar <ianenri@gmail.com>
2017-08-15 15:37:53 -0600

    Workaround for Picker controls

--- KeyboardOverlap/KeyboardOverlap/KeyboardOverlap.Forms.Plugin.iOSUnified/KeyboardOverlapRenderer.cs
+++ KeyboardOverlap/KeyboardOverlap/KeyboardOverlap.Forms.Plugin.iOSUnified/KeyboardOverlapRenderer.cs
@@ -7,134 +7,137 @@ using CoreGraphics;
 using KeyboardOverlap.Forms.Plugin.iOSUnified;
 using System.Diagnostics;
 
-[assembly: ExportRenderer (typeof(Page), typeof(KeyboardOverlapRenderer))]
+[assembly: ExportRenderer(typeof(Page), typeof(KeyboardOverlapRenderer))]
 namespace KeyboardOverlap.Forms.Plugin.iOSUnified
 {
-	[Preserve (AllMembers = true)]
-	public class KeyboardOverlapRenderer : PageRenderer
-	{
-		NSObject _keyboardShowObserver;
-		NSObject _keyboardHideObserver;
-		private bool _pageWasShiftedUp;
-		private double _activeViewBottom;
-		private bool _isKeyboardShown;
+    [Preserve(AllMembers = true)]
+    public class KeyboardOverlapRenderer : PageRenderer
+    {
+        NSObject _keyboardShowObserver;
+        NSObject _keyboardHideObserver;
+        private bool _pageWasShiftedUp;
+        private double _activeViewBottom;
+        private bool _isKeyboardShown;
 
-		public static void Init ()
-		{
-			var now = DateTime.Now;
-			Debug.WriteLine ("Keyboard Overlap plugin initialized {0}", now);
-		}
+        public static void Init()
+        {
+            var now = DateTime.Now;
+            Debug.WriteLine("Keyboard Overlap plugin initialized {0}", now);
+        }
 
-		public override void ViewWillAppear (bool animated)
-		{
-			base.ViewWillAppear (animated);
+        public override void ViewWillAppear(bool animated)
+        {
+            base.ViewWillAppear(animated);
 
-			var page = Element as ContentPage;
+            var page = Element as ContentPage;
 
 			if (page != null) {
-				var contentScrollView = page.Content as ScrollView;
+                var contentScrollView = page.Content as ScrollView;
 
-				if (contentScrollView != null)
-					return;
+                if (contentScrollView != null)
+                    return;
 
-				RegisterForKeyboardNotifications ();
-			}
-		}
+                RegisterForKeyboardNotifications();
+            }
+        }
 
-		public override void ViewWillDisappear (bool animated)
-		{
-			base.ViewWillDisappear (animated);
+        public override void ViewWillDisappear(bool animated)
+        {
+            base.ViewWillDisappear(animated);
 
-			UnregisterForKeyboardNotifications ();
-		}
+            UnregisterForKeyboardNotifications();
+        }
 
-		void RegisterForKeyboardNotifications ()
-		{
-			if (_keyboardShowObserver == null)
-				_keyboardShowObserver = NSNotificationCenter.DefaultCenter.AddObserver (UIKeyboard.WillShowNotification, OnKeyboardShow);
-			if (_keyboardHideObserver == null)
-				_keyboardHideObserver = NSNotificationCenter.DefaultCenter.AddObserver (UIKeyboard.WillHideNotification, OnKeyboardHide);
-		}
+        void RegisterForKeyboardNotifications()
+        {
+            if (_keyboardShowObserver == null)
+                _keyboardShowObserver = NSNotificationCenter.DefaultCenter.AddObserver(UIKeyboard.WillShowNotification, OnKeyboardShow);
+            if (_keyboardHideObserver == null)
+                _keyboardHideObserver = NSNotificationCenter.DefaultCenter.AddObserver(UIKeyboard.WillHideNotification, OnKeyboardHide);
+        }
 
-		void UnregisterForKeyboardNotifications ()
-		{
-			_isKeyboardShown = false;
+        void UnregisterForKeyboardNotifications()
+        {
+            _isKeyboardShown = false;
 			if (_keyboardShowObserver != null) {
-				NSNotificationCenter.DefaultCenter.RemoveObserver (_keyboardShowObserver);
-				_keyboardShowObserver.Dispose ();
-				_keyboardShowObserver = null;
-			}
+                NSNotificationCenter.DefaultCenter.RemoveObserver(_keyboardShowObserver);
+                _keyboardShowObserver.Dispose();
+                _keyboardShowObserver = null;
+            }
 
 			if (_keyboardHideObserver != null) {
-				NSNotificationCenter.DefaultCenter.RemoveObserver (_keyboardHideObserver);
-				_keyboardHideObserver.Dispose ();
-				_keyboardHideObserver = null;
-			}
-		}
+                NSNotificationCenter.DefaultCenter.RemoveObserver(_keyboardHideObserver);
+                _keyboardHideObserver.Dispose();
+                _keyboardHideObserver = null;
+            }
+        }
 
-		protected virtual void OnKeyboardShow (NSNotification notification)
-		{
-			if (!IsViewLoaded || _isKeyboardShown)
-				return;
+        protected virtual void OnKeyboardShow(NSNotification notification)
+        {
+            if (!IsViewLoaded || _isKeyboardShown)
+                return;
 
-			_isKeyboardShown = true;
-			var activeView = View.FindFirstResponder ();
+            _isKeyboardShown = true;
+            var activeView = View.FindFirstResponder();
 
-			if (activeView == null)
-				return;
+            if (activeView == null)
+                return;
 
-			var keyboardFrame = UIKeyboard.FrameEndFromNotification (notification);
-			var isOverlapping = activeView.IsKeyboardOverlapping (View, keyboardFrame);
+            if (activeView.Superview.GetType().Name == nameof(PickerRenderer)) //Picker already shifts layout, so this prevents any shifting issues with UIPickers.
+                return;
 
-			if (!isOverlapping)
-				return;
+            var keyboardFrame = UIKeyboard.FrameEndFromNotification(notification);
+            var isOverlapping = activeView.IsKeyboardOverlapping(View, keyboardFrame);
+
+            if (!isOverlapping)
+                return;
 
 			if (isOverlapping) {
-				_activeViewBottom = activeView.GetViewRelativeBottom (View);
-				ShiftPageUp (keyboardFrame.Height, _activeViewBottom);
-			}
-		}
+                _activeViewBottom = activeView.GetViewRelativeBottom(View);
+                ShiftPageUp(keyboardFrame.Height, _activeViewBottom);
+            }
+        }
 
-		private void OnKeyboardHide (NSNotification notification)
-		{
-			if (!IsViewLoaded)
-				return;
+        private void OnKeyboardHide(NSNotification notification)
+        {
+            if (!IsViewLoaded)
+                return;
 
-			_isKeyboardShown = false;
-			var keyboardFrame = UIKeyboard.FrameEndFromNotification (notification);
+            _isKeyboardShown = false;
+            var keyboardFrame = UIKeyboard.FrameEndFromNotification(notification);
 
 			if (_pageWasShiftedUp) {
-				ShiftPageDown (keyboardFrame.Height, _activeViewBottom);
-			}
-		}
+                ShiftPageDown(keyboardFrame.Height, _activeViewBottom);
+            }
+        }
 
-		private void ShiftPageUp (nfloat keyboardHeight, double activeViewBottom)
-		{
-			var pageFrame = Element.Bounds;
+        private void ShiftPageUp(nfloat keyboardHeight, double activeViewBottom)
+        {
+            var pageFrame = Element.Bounds;
 
-			var newY = pageFrame.Y + CalculateShiftByAmount (pageFrame.Height, keyboardHeight, activeViewBottom);
+            var newY = pageFrame.Y + CalculateShiftByAmount(pageFrame.Height, keyboardHeight, activeViewBottom);
 
-			Element.LayoutTo (new Rectangle (pageFrame.X, newY,
-				pageFrame.Width, pageFrame.Height));
+            Element.LayoutTo (new Rectangle (pageFrame.X, newY,
+                pageFrame.Width, pageFrame.Height));
 
-			_pageWasShiftedUp = true;
-		}
+            _pageWasShiftedUp = true;
+        }
 
-		private void ShiftPageDown (nfloat keyboardHeight, double activeViewBottom)
-		{
-			var pageFrame = Element.Bounds;
+        private void ShiftPageDown (nfloat keyboardHeight, double activeViewBottom)
+        {
+            var pageFrame = Element.Bounds;
 
-			var newY = pageFrame.Y - CalculateShiftByAmount (pageFrame.Height, keyboardHeight, activeViewBottom);
+            var newY = pageFrame.Y - CalculateShiftByAmount(pageFrame.Height, keyboardHeight, activeViewBottom);
 
-			Element.LayoutTo (new Rectangle (pageFrame.X, newY,
-				pageFrame.Width, pageFrame.Height));
+            Element.LayoutTo (new Rectangle (pageFrame.X, newY,
+                pageFrame.Width, pageFrame.Height));
 
-			_pageWasShiftedUp = false;
-		}
+            _pageWasShiftedUp = false;
+        }
 
-		private double CalculateShiftByAmount (double pageHeight, nfloat keyboardHeight, double activeViewBottom)
-		{
-			return (pageHeight - activeViewBottom) - keyboardHeight;
-		}
-	}
+        private double CalculateShiftByAmount (double pageHeight, nfloat keyboardHeight, double activeViewBottom)
+        {
+            return (pageHeight - activeViewBottom) - keyboardHeight;
+        }
+    }
 }
